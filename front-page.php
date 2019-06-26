dd70f1f0767def55ef975c8ed79a13234ffad889
dzenanh9 <45795609+dzenanh9@users.noreply.github.com>
2019-06-26 14:27:16 +0200

    Signed-off-by: dzenanh9 <45795609+dzenanh9@users.noreply.github.com>

--- front-page.php
+++ front-page.php
@@ -7,15 +7,20 @@
 
     <div class="service">
         <div id="service_1">
-        <div class="service_overlay"></div>
+            <div class="service_overlay"></div>
             <h1><div id="spec_img"></div> Pantheon</h1>
-            <h2><svg xmlns="http://www.w3.org/2000/svg" width="50" height="2" viewBox="0 0 50 2" transform="translate(-20 -7)"><line x2="50"  fill="none" stroke="#4d6094" stroke-width="2vh"/></svg>Za jednostavno vođenje preduzeća</h2>
+            <h2><svg xmlns="http://www.w3.org/2000/svg" width="50" height="2" viewBox="0 0 50 2" transform="translate(-20 -5)"><line x2="50"  fill="none" stroke="#4d6094" stroke-width="2vh"/></svg>Za jednostavno vođenje preduzeća</h2>
         </div>
     </div>
     
 
 
-    
+    <div class="service_slider_controls">
+        <div class="switch_left"><svg xmlns="http://www.w3.org/2000/svg" width="26.981" height="30.732" viewBox="0 0 26.981 30.732"><g transform="translate(3649.661 1267.366) rotate(180)"><path d="M0,0H30" transform="translate(3623.18 1237.5) rotate(30)" fill="none" stroke="#f3f3f3" stroke-width="2"/><line x2="30" transform="translate(3623.179 1266.5) rotate(-30)" fill="none" stroke="#f3f3f3" stroke-width="2"/></g></svg></div>
+        <div class="next_service">Networking</div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="400" height="2" viewBox="0 0 50 2" transform="translate(-90, 20)"><line x2="350"  fill="none" stroke="#A4A4A4" stroke-width="2"/></svg>
+        <div class="switch_right"><svg xmlns="http://www.w3.org/2000/svg" width="26.981" height="30.732" viewBox="0 0 26.981 30.732"><g transform="translate(-3622.68 -1236.634)"><path d="M0,0H30" transform="translate(3623.18 1237.5) rotate(30)" fill="none" stroke="#f3f3f3" stroke-width="2"/><line x2="30" transform="translate(3623.179 1266.5) rotate(-30)" fill="none" stroke="#f3f3f3" stroke-width="2"/></g></svg></div>
+    </div>
 </div>
 
 
--- header.php
+++ header.php
@@ -25,7 +25,23 @@
             <img src="<?php echo bloginfo('template_url');?>/images/ser1.jpg" alt="">
         </div>
         <div class="service_detail_desc">
-            
+            <div class="detail_info">
+                <div class="detail_index">
+                01
+                </div>
+                <div class="detail_small_title">
+                    Small Business
+                </div>
+            </div>
+            <div class="detail_tag_line">Za samostalne poduzetnike i mala preduzeća</div>
+
+            <div class="detail_controls">
+                <div class="detail_expand">Pročitaj više <svg xmlns="http://www.w3.org/2000/svg" width="50" height="2" viewBox="0 0 50 2" transform="translate(0 -5)"><line x2="50"  fill="none" stroke="#F3F3F3" stroke-width="2"/></svg></div>
+                <div class="detail_switch_controls">
+                    <div class="switch_left"><svg xmlns="http://www.w3.org/2000/svg" width="26.981" height="30.732" viewBox="0 0 26.981 30.732"><g transform="translate(3649.661 1267.366) rotate(180)"><path d="M0,0H30" transform="translate(3623.18 1237.5) rotate(30)" fill="none" stroke="#f3f3f3" stroke-width="2"/><line x2="30" transform="translate(3623.179 1266.5) rotate(-30)" fill="none" stroke="#f3f3f3" stroke-width="2"/></g></svg></div>
+                    <div class="switch_right"><svg xmlns="http://www.w3.org/2000/svg" width="26.981" height="30.732" viewBox="0 0 26.981 30.732"><g transform="translate(-3622.68 -1236.634)"><path d="M0,0H30" transform="translate(3623.18 1237.5) rotate(30)" fill="none" stroke="#f3f3f3" stroke-width="2"/><line x2="30" transform="translate(3623.179 1266.5) rotate(-30)" fill="none" stroke="#f3f3f3" stroke-width="2"/></g></svg></div>
+                </div>
+            </div>
         </div>
 </div>
 
